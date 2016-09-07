5ac3f7b41c2c2ec3ea9eeaafbee58c5203f56091
Lovino Maxime <maximelovino@gmail.com>
2016-09-07 21:33:36 +0200

    added myLocation to maps view

--- WaspDroid/app/src/main/java/ch/hepia/waspmasterrace/waspdroid/MapsActivity.java
+++ WaspDroid/app/src/main/java/ch/hepia/waspmasterrace/waspdroid/MapsActivity.java
@@ -1,7 +1,14 @@
 package ch.hepia.waspmasterrace.waspdroid;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private Run run;
+    private final int REQUEST_LOCATION_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        run = (Run)getIntent().getSerializableExtra("RUN");
+        run = (Run) getIntent().getSerializableExtra("RUN");
         mapFragment.getMapAsync(this);
     }
 
@@ -44,6 +52,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         ArrayList<GPScoordinates> points = run.getSortedListOfPoints();
         //mMap.setMyLocationEnabled(true);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("hello, permission granted");
+            mMap.setMyLocationEnabled(true);
+        } else {
+            System.out.println("hello from the poors");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION_CODE);
+        }
 
         if (!points.isEmpty()) {
             LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -62,6 +79,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addPolyline(poly);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(centerMaps));
             mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+        }
+    }
+
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_LOCATION_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            mMap.setMyLocationEnabled(true);
         }
     }
 }
